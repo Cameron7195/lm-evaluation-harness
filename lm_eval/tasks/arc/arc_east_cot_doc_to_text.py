ccffce13f831c60189da64dd42c4c3d13682b098
Alexander Detkov <alexander.d.detkov@gmail.com>
2024-08-23 23:03:43 +0000

    add new tasks

---
+++ lm_eval/tasks/arc/arc_east_cot_doc_to_text.py
@@ -0,0 +1,9 @@
+def doc_to_text(doc):
+    question = doc["question"]
+    choices = doc["choices"]
+    
+    #mc = " ".join([f"({label}) {text}" for label, text in zip(choices['label'], choices['text'])])
+    #return f"Given the following problem, reason and give a final answer to the problem.\nProblem: {question} {mc}\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n"
+
+    mc = "\n".join([f"{label}. {text}" for label, text in zip(choices['label'], choices['text'])])
+    return f"Given the following problem, reason and give a final answer to the problem.\nProblem: {question}\n{mc}\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n"
